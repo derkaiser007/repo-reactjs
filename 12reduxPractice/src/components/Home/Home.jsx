47a259a81111ee74378df6a6c8c9395bdba334fa
derkaiser007 <nirajshrivastav01@gmail.com>
2024-03-08 23:46:51 +0530

    added reactjs project

--- 12reduxPractice/src/components/Home/Home.jsx
+++ 12reduxPractice/src/components/Home/Home.jsx
@@ -8,17 +8,6 @@ function Home({setValue}){
     const [input, setInput] = React.useState('')
     const navigate = useNavigate();
 
-    React.useEffect(() => {
-        const data = window.localStorage.getItem('my_value')
-        if (data) {
-            setInput(JSON.parse(data))
-        }
-    }, [])
-
-    React.useEffect(() => {
-        window.localStorage.setItem('my_value', JSON.stringify(input))
-    })
-
     const onSubmitHandler = () => {
         setValue(input); 
         navigate('/about'); 
